9ad58bb40dc66f92aac92ccfd50fbde0136586ab
Nikita Timofeev <stariy95@gmail.com>
2018-06-07 11:32:14 +0300

    Support for Optional<?> injection

--- guice-over-bootique-di/src/main/java/io/bootique/di/spi/OptionalBinderAdapter.java
+++ guice-over-bootique-di/src/main/java/io/bootique/di/spi/OptionalBinderAdapter.java
@@ -6,18 +6,27 @@ import com.google.inject.binder.LinkedBindingBuilder;
 public class OptionalBinderAdapter<T> {
 
     private final BinderAdapter guiceBinderAdapter;
+    private final OptionalBindingBuilderAdapter<T> adapter;
     private final Key<T> key;
 
     public OptionalBinderAdapter(com.google.inject.Binder guiceBinder, Key<T> key) {
         this.key = key;
         this.guiceBinderAdapter = (BinderAdapter) guiceBinder;
+        this.adapter = createBindingBuilder();
     }
 
     public LinkedBindingBuilder<T> setDefault() {
-        return new BindingBuilderAdapter<>(guiceBinderAdapter, ConversionUtils.toBootiqueKey(key));
+        return adapter;
     }
 
     public LinkedBindingBuilder<T> setBinding() {
-        return new BindingBuilderAdapter<>(guiceBinderAdapter, ConversionUtils.toBootiqueKey(key));
+        return adapter;
+    }
+
+    private OptionalBindingBuilderAdapter<T> createBindingBuilder() {
+        OptionalBindingBuilderAdapter<T> adapter = new OptionalBindingBuilderAdapter<>(
+                guiceBinderAdapter, ConversionUtils.toBootiqueKey(key));
+        guiceBinderAdapter.getInjectorAdapter().registerBindingBuilder(adapter);
+        return adapter;
     }
 }
